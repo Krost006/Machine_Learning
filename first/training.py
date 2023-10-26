d957c8579594097806c685e3ae227cd28ba62625
Krost006 <63459047+Krost006@users.noreply.github.com>
2023-10-26 09:25:35 +0300

    Third try
    Training imports added

--- first/training.py
+++ first/training.py
@@ -1,4 +1,7 @@
 import torch.optim as optim
+from model import *
+from dataset_prepare import *
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
